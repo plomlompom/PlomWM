646b1871cc4a7eece4399540e09d9156caf14138
Christian Heller <c.heller@plomlompom.de>
2011-12-25 07:31:43 +0100

    Repaired EnterNotify checks, kicked out ChangeFocus checks again.

--- plomwm.c
+++ plomwm.c
@@ -1,4 +1,4 @@
-/* PlomWM 0.4.1 / written by Christian Heller <c.heller@plomlompom.de> / based on Nick Welch's TinyWM */
+/* PlomWM 0.4.2 / written by Christian Heller <c.heller@plomlompom.de> / based on Nick Welch's TinyWM */
 #include <stdlib.h>
 #include <X11/Xlib.h>
 #include <X11/Xatom.h>
@@ -20,9 +20,6 @@ int main(void) {
   short *         r_geometry;
   short           a_geometry[4];
 
-  /* Store the correct focus in here, for focus stealing prevention. */
-  Window focus;
-
   /* Get fullscreen geometry. */
   XWindowAttributes x;
   XGetWindowAttributes(dpy, root, &x);
@@ -30,7 +27,7 @@ int main(void) {
   int full_height = x.height;
 
   /* Select / grab certain window and user action events. */
-  XSelectInput(dpy, root, SubstructureNotifyMask | EnterWindowMask | FocusChangeMask);
+  XSelectInput(dpy, root, SubstructureNotifyMask | EnterWindowMask);
   XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym("F11")), Mod4Mask,           root, True, GrabModeAsync, GrabModeAsync);
   XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym("Tab")), Mod4Mask,           root, True, GrabModeAsync, GrabModeAsync);
   XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym("Tab")), Mod4Mask|ShiftMask, root, True, GrabModeAsync, GrabModeAsync);
@@ -107,18 +104,16 @@ int main(void) {
        XChangeProperty(dpy, ev.xconfigure.window, Geometry, XA_INTEGER, 16,
                        PropModeReplace, (unsigned char *) &a_geometry, 4); } }
 
-    /* If a window is created, store its fullscreen property, assuming it to be negative. */
+    /* Store new windows' fullscreen properties (assume them to be negative), request their EnterNotify events. */
     else if (ev.type == CreateNotify) {
+      XSetWindowAttributes y;
+      y.event_mask = EnterWindowMask;
+      XChangeWindowAttributes(dpy, ev.xcreatewindow.window, CWEventMask, &y);
       a_fullscreen[0] = 0;
       XChangeProperty(dpy, ev.xcreatewindow.window, IsFullscreen, XA_INTEGER, 8, 
                       PropModeReplace, (unsigned char *) &a_fullscreen, 1); } 
 
     /* Force focus-follows-pointer to prevent focus stealing. */
     else if (ev.type == EnterNotify) {
-      focus = ev.xcrossing.window;
-      XSetInputFocus(dpy, focus, None, CurrentTime); } 
-
-    /* Detect and correct illegal FocusOut events to prevent focus stealing. */
-    else if (ev.type == FocusOut) {
-     if (focus && ev.xfocus.window == focus) {
-       XSetInputFocus(dpy, focus, None, CurrentTime); } } } }
+      Window focus = ev.xcrossing.window;
+      if (focus) XSetInputFocus(dpy, focus, None, CurrentTime); } } }
